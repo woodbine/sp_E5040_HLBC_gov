0cfa8dad006eb05d595d868368a5cfb73d974315
woodbine <ian@spendnetwork.com>
2015-04-17 20:19:39 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -30,6 +30,7 @@ for link in links:
 		if '.csv' in url:
 			url = 'http://www.havering.gov.uk'+url
 			title = link.contents[0].encode('utf8')
+			title = unicode(title)
 			print title[0:13]
 			# create the right strings for the new filename
 			csvYr = title.split(' ')[1]
